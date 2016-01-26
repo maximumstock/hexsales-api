47f4e055d4fe5dcb66d66a8e8bda45043385fd62
Maximilian Stock <max@crispcode.de>
2016-01-26 03:16:32 +0100

    added dev-dependencies and commands

---
+++ test/0_unit/test.example.js
@@ -0,0 +1,15 @@
+'use strict';
+
+require('co-mocha');
+const expect = require('chai').expect;
+
+
+describe('example', function() {
+
+    it('should do something', function*() {
+
+        expect(true);
+
+    });
+
+})
