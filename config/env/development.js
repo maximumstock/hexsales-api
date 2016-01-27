621b9ccba87bb5693cbf36a828d75dfdcb0fbb67
Maximilian Stock <max@crispcode.de>
2016-01-27 22:50:41 +0100

    added configuration testing

--- config/env/development.js
+++ config/env/development.js
@@ -4,4 +4,14 @@
  * @file Exports a configuration object specific to environment 'development'
  */
 
-module.exports = {};
+module.exports = {
+
+    environment: 'development',
+
+    dbname: 'hexdatabaseapi',
+    dbuser: 'hexdatabaseapi',
+    dbpass: 'hexdatabaseapi',
+    dbhost: 'localhost',
+    dbport: 5432,
+
+};
--- config/env/production.js
+++ config/env/production.js
@@ -4,4 +4,30 @@
  * @file Exports a configuration object specific to environment 'production'
  */
 
-module.exports = {};
+if (!process.env.PORT)  {
+    throw new Error('process.env.PORT not provided');
+}
+if (!process.env.DBHOST)  {
+    throw new Error('process.env.DBHOST not provided');
+}
+if (!process.env.DBNAME)  {
+    throw new Error('process.env.DBNAME not provided');
+}
+if (!process.env.DBUSER)  {
+    throw new Error('process.env.DBUSER not provided');
+}
+if (!process.env.DBPASS)  {
+    throw new Error('process.env.DBPASS not provided');
+}
+
+module.exports = {
+
+    environment: 'production',
+
+    port: process.env.PORT,
+    dbhost: process.env.DBHOST,
+    dbname: process.env.DBNAME,
+    dbuser: process.env.DBUSER,
+    dbpass: process.env.DBPASS
+
+};
