c4811f355c0929cc3ddcf204fc79d7ea0c89e1a5
Maximilian Stock <me@maximumstock.net>
2016-04-13 16:17:41 +0200

    last try to save the ship; added working concurrent refreshs

--- migrations/20160110040916_sales.js
+++ migrations/20160110040916_sales.js
@@ -35,18 +35,26 @@ exports.up = function(knex, Promise) {
                      from sales \
                      group by name, uuid, internal, type, rarity, setid, aa, ea;'),
 
+    knex.schema.raw('create unique index u_idx on distinct_articles(internal, aa, ea, type);'),
+
     // daily history for each article
     knex.schema.raw('create materialized view daily_article_sales as \
                      select name, internal, date, currency, sum(1) as daily_quantity, sum(price) as daily_total \
                      from sales \
                      group by name, internal, date, currency;'),
 
+
+    knex.schema.raw('create unique index u_idx2 on daily_article_sales(name, date, currency);'),
+
     // daily history of all articles
     knex.schema.raw('create materialized view economy_daily_history as \
                      select date, rarity, currency, count(1)::int as quantity, sum(price)::int as total, avg(price)::int as avg, min(price) as min, max(price) as max \
                      from sales \
                      group by currency, date, rarity \
-                     order by date, currency, rarity;')
+                     order by date, currency, rarity;'),
+
+
+    knex.schema.raw('create unique index u_idx3 on economy_daily_history(date, rarity, currency);'),
 
 
   ]);
