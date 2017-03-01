4420c99cf08e50ced67495104889990b9aa1960b
Maximilian Stock <me@maximumstock.net>
2017-03-01 02:06:25 +0100

    change updater to use alternative AH dump source

--- config/index.js
+++ config/index.js
@@ -9,8 +9,7 @@ module.exports = {
     port: process.env.PORT ||  3000,
     dbUrl: process.env.DBURL || 'pg://hexsalesapi:hexsalesapi@localhost:5432/hexsalesapi',
 
-    cdnIndex: 'http://dl.hex.gameforge.com/auctionhouse/index.txt',
-    cdnRoot: 'http://dl.hex.gameforge.com/auctionhouse',
+    cdnRoot: 'https://hexpvptools.net/ah_dump/',
     apiUrl: 'http://hexdbapi.hexsales.net/v1/objects/search',
 
     startDate: '2016-07-08',
